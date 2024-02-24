abfe0a446b6dec9708f304ec394d1f2524a477b5
Troan89 <Wiki-Mars@yandex.ru>
2024-02-24 10:01:03 +0400

    Updates

---
+++ static/js/747.57fc54e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[747],{6747:function(r,a,u){u.r(a);u(390);var e=u(2559);a.default=function(){return(0,e.jsx)("div",{children:"Error404"})}}}]);
+//# sourceMappingURL=747.57fc54e0.chunk.js.map
