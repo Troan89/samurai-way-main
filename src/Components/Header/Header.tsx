85687b512b7e11082e3ce67e5a0b325d1222c46c
Troan89 <Wiki-Mars@yandex.ru>
2024-05-06 15:23:39 +0400

    fix style header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import s from './Header.module.css'
 import {NavLink} from "react-router-dom";
-import {useAppDispatch, useAppSelector} from "../../state/redux-store";
-import {LogOut} from "../../state/AuthReducer";
 
 type Header_T = {
     isAuth: boolean | null
@@ -11,19 +9,18 @@ type Header_T = {
 }
 
 export const Header = ({isAuth, login, LogOut}: Header_T) => {
-    const dispatch = useAppDispatch()
     const logOutHandler = () => {
         LogOut()
     }
 
     return (
         <header className={s.header}>
-            <img
+            <img alt={''}
                 src={"https://avatars.mds.yandex.net/i?id=efd6ab8b3a67b0abc777cefaf348c9e086e53173-9265516-images-thumbs&n=13"}/>
             <div className={s.loginBlock}>
+                <div>{login}</div>
                 {isAuth
                     ? <button onClick={logOutHandler}>LogOut</button> : <NavLink to={'/login'}>Login</NavLink>}
-                <div>{login}</div>
             </div>
 
         </header>
