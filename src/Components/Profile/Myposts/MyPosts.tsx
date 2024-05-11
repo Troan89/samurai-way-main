57635489c7769112ff630cb222c8c9968c546d8f
Troan89 <Wiki-Mars@yandex.ru>
2024-05-11 09:23:22 +0400

    изменение блока профайл. изменение блока мои посты

--- src/Components/Profile/Myposts/MyPosts.tsx
+++ src/Components/Profile/Myposts/MyPosts.tsx
@@ -12,7 +12,7 @@ export const MyPost = (props: MePostPropsType) => {
     let postsElement = props.posts.map((post, index) => <Post key={index} message={post.message} like={post.like}/>)
     return (
         <div className={s.postsBlock}>
-            My post
+            Мои посты
             <FormPost />
             <div className={s.posts}>
                 {postsElement}
@@ -21,66 +21,3 @@ export const MyPost = (props: MePostPropsType) => {
     )
 }
 
-
-//
-// import React, {ChangeEvent, useState} from "react";
-// import { addPostAC, updateNewPostTextAC } from "../../../state/profileReducer";
-// import {ActionType, PostsType, ProfilePageType,} from "../../../state/State";
-// import s from './MyPosts.module.css'
-// import {Post} from "./Post/Post";
-// import {useDispatch, useSelector} from "react-redux";
-// import {AppRootStateType} from "../../../state/redux-store";
-//
-//
-//
-// export type PostsDataType = {
-//     id: string
-//     message: string
-//     like: number
-// }
-// type MePostPropsType = {
-//     // postsData: PostsDataType[]
-//     // dispatch:(action:ActionType)=>void
-//     // newPostText: string
-// }
-//
-// export const MyPost = (props: MePostPropsType) => {
-//     const [postText, setPostText] = useState<string>('')
-//
-//     let dispatch = useDispatch()
-//     let posts = useSelector<AppRootStateType, PostsType[]>(state=>state.profilePage.postsData)
-//
-//     // let postsElement = props.postsData.map((post, index) => <Post key={index} message={post.message} like={post.like}/>)
-//     let postsElement = posts.map((post, index) => <Post key={index} message={post.message} like={post.like}/>)
-//
-//     const addPost = () => {
-//         // props.dispatch(addPostAC())
-//         dispatch(addPostAC())
-//         setPostText('')
-//     }
-//     const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-//         setPostText(e.currentTarget.value)
-//         // props.dispatch(updateNewPostTextAC(postText))
-//         dispatch(updateNewPostTextAC(postText))
-//     }
-//
-//     return (
-//         <div className={s.postsBlock}>
-//             My post
-//             <div>
-//                 <div>
-//                     <textarea
-//                         value={postText}
-//                         onChange={onPostChange}
-//                     />
-//                 </div>
-//                 <div>
-//                     <button onClick={addPost}>Добавить пост</button>
-//                 </div>
-//             </div>
-//             <div className={s.posts}>
-//                 {postsElement}
-//             </div>
-//         </div>
-//     )
-// }
--- src/Components/Profile/Myposts/Post/Post.tsx
+++ src/Components/Profile/Myposts/Post/Post.tsx
@@ -9,8 +9,8 @@ type PostPropsType = {
 export const Post = (props:PostPropsType) => {
     return (
         <div className={s.item}>
-            <img src="https://avatars.mds.yandex.net/i?id=1dd689a2061aafd4147897ff59e5ee46e78084ca-9181203-images-thumbs&n=13" />
-            {props.message}
+            <img alt={''} src="https://avatars.mds.yandex.net/i?id=1dd689a2061aafd4147897ff59e5ee46e78084ca-9181203-images-thumbs&n=13" />
+            <span  className={s.post}>{props.message}</span>
             <div>
                 <span>{props.like}</span>
             </div>
